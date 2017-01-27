c76eb08ba26cf01cdf15030dbea33a4235401d74
suragch <studymongolian@gmail.com>
2017-01-27 11:00:04 +0800

    made a start with vertical text

--- mongol-library/src/main/java/net/studymongolian/mongollibrary/MongolTextLine.java
+++ mongol-library/src/main/java/net/studymongolian/mongollibrary/MongolTextLine.java
@@ -1,7 +1,142 @@
 package net.studymongolian.mongollibrary;
 
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.text.TextPaint;
+import android.util.Log;
+
 public class MongolTextLine {
+    private static final boolean DEBUG = true;
+
+    private TextPaint mPaint;
+    private CharSequence mText;
+
+    private static final MongolTextLine[] sCached = new MongolTextLine[3];
+
+    static MongolTextLine obtain() {
+        MongolTextLine tl;
+        synchronized (sCached) {
+            for (int i = sCached.length; --i >= 0;) {
+                if (sCached[i] != null) {
+                    tl = sCached[i];
+                    sCached[i] = null;
+                    return tl;
+                }
+            }
+        }
+        tl = new MongolTextLine();
+        if (DEBUG) {
+            Log.v("TLINE", "new: " + tl);
+        }
+        return tl;
+    }
+
+    static MongolTextLine recycle(MongolTextLine tl) {
+        tl.mText = null;
+        tl.mPaint = null;
+        synchronized(sCached) {
+            for (int i = 0; i < sCached.length; ++i) {
+                if (sCached[i] == null) {
+                    sCached[i] = tl;
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Initializes a MongolTextLine and prepares it for use.
+     *
+     * @param paint the base paint for the line
+     * @param text the text, can be Styled
+     */
+    void set(TextPaint paint, CharSequence text) {
+        mPaint = paint;
+        mText = text;
+
+    }
+
+    /**
+     * Renders the TextLine.
+     *
+     * @param c the canvas to render on
+     * @param x the leading margin position
+     * @param top the top of the line
+     * @param y the baseline
+     * @param bottom the bottom of the line
+     */
+    void draw(Canvas c, float x, int top, int y, int bottom) {
+
+    }
+
+    /**
+     * Returns metrics information for the entire line.
+     *
+     * @param fmi receives font metrics information, can be null
+     * @return the signed width of the line
+     */
+    float metrics(Paint.FontMetricsInt fmi) {
+        return 0;
+    }
+
+    /**
+     * Returns information about a position on the line.
+     *
+     * @param offset the line-relative character offset, between 0 and the
+     * line length, inclusive
+     * @param trailing true to measure the trailing edge of the character
+     * before offset, false to measure the leading edge of the character
+     * at offset.
+     * @param fmi receives metrics information about the requested
+     * character, can be null.
+     * @return the signed offset from the leading margin to the requested
+     * character edge.
+     */
+    float measure(int offset, boolean trailing, Paint.FontMetricsInt fmi) {
+
+        return 0;
+    }
+
+    /**
+     * Draws a unidirectional (but possibly multi-styled) run of text.
+     *
+     *
+     * @param c the canvas to draw on
+     * @param start the line-relative start
+     * @param limit the line-relative limit
+     * @param x the position of the run that is closest to the leading margin
+     * @param top the top of the line
+     * @param y the baseline
+     * @param bottom the bottom of the line
+     * @param needWidth true if the width value is required.
+     * @return the signed width of the run, based on the paragraph direction.
+     * Only valid if needWidth is true.
+     */
+    private float drawRun(Canvas c, int start,
+                          int limit, float x, int top, int y, int bottom,
+                          boolean needWidth) {
+
+        return 0;
+    }
+
+    /**
+     * Measures a unidirectional (but possibly multi-styled) run of text.
+     *
+     *
+     * @param start the line-relative start of the run
+     * @param offset the offset to measure to, between start and limit inclusive
+     * @param limit the line-relative limit of the run
+     * @param fmi receives metrics information about the requested
+     * run, can be null.
+     * @return the signed width from the start of the run to the leading edge
+     * of the character at offset, based on the run (not paragraph) direction
+     */
+    private float measureRun(int start, int offset, int limit, Paint.FontMetricsInt fmi) {
+        return 0;
+    }
+
 
 
 }
--- mongol-library/src/main/java/net/studymongolian/mongollibrary/MongolTextView.java
+++ mongol-library/src/main/java/net/studymongolian/mongollibrary/MongolTextView.java
@@ -1,6 +1,10 @@
 package net.studymongolian.mongollibrary;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -8,11 +12,83 @@ import android.view.View;
 
 public class MongolTextView extends View {
 
+    String mText = "g This is some 文字 . 😊";
+    TextPaint mTextPaint;
+    Paint mPaint;
+    //StaticLayout mStaticLayout;
+
+    // use this constructor if creating MyView programmatically
     public MongolTextView(Context context) {
         super(context);
+        initLabelView();
     }
 
+    // this constructor is used when created from xml
     public MongolTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initLabelView();
+    }
+
+    private void initLabelView() {
+        mTextPaint = new TextPaint();
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setTextSize(26 * getResources().getDisplayMetrics().density);
+        mTextPaint.setColor(0xFF000000);
+
+        mPaint = new Paint();
+        mPaint.setColor(Color.WHITE);
+        mPaint.setStyle(Paint.Style.FILL);
+        // default to a single line of text
+        int width = (int) mTextPaint.measureText(mText);
+
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Tell the parent layout how big this view would like to be
+        // but still respect any requirements (measure specs) that are passed down.
+        // determine the width
+        int width;
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthRequirement = MeasureSpec.getSize(widthMeasureSpec);
+        if (widthMode == MeasureSpec.EXACTLY) {
+            width = widthRequirement;
+        } else {
+            width = (int) mTextPaint.measureText(mText) + getPaddingLeft() + getPaddingRight();
+            if (widthMode == MeasureSpec.AT_MOST) {
+                if (width > widthRequirement) {
+                    width = widthRequirement;
+                }
+            }
+        }
+
+        // determine the height
+        int height;
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightRequirement = MeasureSpec.getSize(heightMeasureSpec);
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightRequirement;
+        } else {
+            height = 50 + getPaddingTop() + getPaddingBottom();
+            if (heightMode == MeasureSpec.AT_MOST) {
+                height = Math.min(height, heightRequirement);
+            }
+        }
+
+        // Required call: set width and height
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.save();
+        canvas.translate(getPaddingLeft(), getPaddingTop());
+        canvas.rotate(90);
+        canvas.drawText(mText, 0, 0, mTextPaint);
+        canvas.drawCircle(0, 0, 10, mPaint);
+        canvas.restore();
     }
 }
