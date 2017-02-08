f8b4e41b1a227ad978b39f6969a056216486e33b
suragch <studymongolian@gmail.com>
2017-02-08 18:08:33 +0800

    created main menu and first demo

--- demo-app/src/main/java/net/studymongolian/mongollibrarydemo/MainActivity.java
+++ demo-app/src/main/java/net/studymongolian/mongollibrarydemo/MainActivity.java
@@ -1,18 +1,48 @@
 package net.studymongolian.mongollibrarydemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import net.studymongolian.mongollibrary.MongolTextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements MainActivityRecyclerViewAdapter.ItemClickListener {
+
+    MainActivityRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View v = new MongolTextView(getApplicationContext());
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("MongolLabel");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvApiDemoList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MainActivityRecyclerViewAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+
+        switch (position) {
+            case 0: // MongolLabel
+                Intent intent = new Intent(this, MongolLabelActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
