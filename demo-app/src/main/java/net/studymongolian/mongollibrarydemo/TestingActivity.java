5afa76309986f0b8c13bbfc35eac85d495d964a2
suragch <studymongolian@gmail.com>
2017-10-06 10:26:48 +0800

    hiding the system keyboard testing

--- demo-app/src/main/java/net/studymongolian/mongollibrarydemo/TestingActivity.java
+++ demo-app/src/main/java/net/studymongolian/mongollibrarydemo/TestingActivity.java
@@ -11,11 +11,15 @@ import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -29,10 +33,18 @@ import net.studymongolian.mongollibrary.MongolInputMethodManager;
 import net.studymongolian.mongollibrary.MongolTextView;
 
 import java.io.File;
+import java.util.ArrayList;
 
 
 public class TestingActivity extends AppCompatActivity {
 
+
+    RecyclerView rvResults;
+//    SearchWordListAdapter mWordAdapter;
+//    SearchVerseResultsAdapter mVerseAdapter;
+    int searchScope;
+    ArrayList<String> searchTerms;
+    StringBuilder currentTerm;
     MongolEditText editText;
 
     @Override
@@ -40,33 +52,41 @@ public class TestingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing);
 
-        ImeContainer imeContainer = (ImeContainer) findViewById(R.id.keyboard);
-        editText = findViewById(R.id.test_mtv);
+
+        editText = findViewById(R.id.met_input_box);
+        ImeContainer keyboard = findViewById(R.id.ime_container);
 
         MongolInputMethodManager mimm = new MongolInputMethodManager();
         mimm.addEditor(editText);
-        mimm.setIme(imeContainer);
+        mimm.setIme(keyboard);
         mimm.setAllowSystemSoftInput(MongolInputMethodManager.NO_EDITORS);
         mimm.startInput();
 
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        searchTerms = new ArrayList<>();
+        currentTerm = new StringBuilder();
+        //searchScope = Book.SEARCH_ALL;
 
-            }
+        // listen for keyboard input changes
+        editText.addTextChangedListener(mTextWatcher);
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                Log.i("TAG", "onTextChanged: " + charSequence);
-            }
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM,
+//                WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
 
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
     }
 
+    private TextWatcher mTextWatcher = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            Log.i("TAG", "onTextChanged: " + currentTerm);
+            //new GetWordsThatStartWith().execute(charSequence.toString());
+        }
+
+        // these do nothing
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+        @Override
+        public void afterTextChanged(Editable editable) {}
+    };
 
     public void onButtonClick(View view) {
         //textView.setTextSize(56);
