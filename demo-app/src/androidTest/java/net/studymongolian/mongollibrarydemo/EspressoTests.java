2b0763b2cfca84d1a735f697650b473f8214690d
suragch <studymongolian@gmail.com>
2018-08-22 18:30:46 +0800

    bug fix

--- demo-app/src/androidTest/java/net/studymongolian/mongollibrarydemo/EspressoTests.java
+++ demo-app/src/androidTest/java/net/studymongolian/mongollibrarydemo/EspressoTests.java
@@ -5,7 +5,6 @@ import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -32,7 +31,6 @@ import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.is;
 
-@LargeTest
 @RunWith(AndroidJUnit4.class)
 public class EspressoTests {
 
